135e23acee73d494820ce1988d2630c976469acc
Hmin25 <69612620+Hmin25@users.noreply.github.com>
2021-11-23 20:55:32 +0800

    Done

--- src/ReservationForm.js
+++ src/ReservationForm.js
@@ -3,18 +3,48 @@ import Logo from "./logo192.png";
 import { useState } from "react";
 import InputBar from "./Components/InputBar";
 import DropDownBar from "./Components/DropDownBar";
+import ReactPhoneInput from "react-phone-number-input";
+import 'react-phone-number-input/style.css'
 
 export default function ReservationForm() {
   const [fullName, setFullName] = useState(null);
   const [email, setEmail] = useState(null);
   const [agentID, setAgentID] = useState(null);
-  const [phoneNumber, setPhoneNumber] = useState(null);
+  const [phoneNumber, setPhoneNumber] = useState();
   const [timeLimitOptions, setTimeLimitOptions] = useState([]);
 
   const testingData = ["1hour", "2hours", "3hour"];
 
+  const phoneInputWithCountryCodes = () => {
+
+    return (
+      <div>
+        <ReactPhoneInput
+          international={true}
+          placeholder="eg: 016-12345467"
+          defaultCountry={"MY"}
+          value={phoneNumber}
+          // onChange={(e) => {setPhoneNumber(); console.log(e.target.value)}}
+          onChange={setPhoneNumber}
+          className="phoneInput"
+          // className="form-control"
+        />
+      </div>
+    );
+  };
+  console.log(phoneNumber)
+
   return (
     <>
+      {/* Breadcrumbs */}
+      <ul className="breadcrumb">
+        <li>
+          <a href="/keymap">Floorplan Board</a>
+        </li>
+        <li>Reserve</li>
+      </ul>
+
+      {/* Content start */}
       <div className="d-flex justify-content-center mt-auto">
         <div className="reserveForm">
           {/* First row */}
@@ -33,15 +63,16 @@ export default function ReservationForm() {
             </div>
             <div className="row mt-3 mx-3">
               <InputBar setCurrentData={setAgentID} title="Agent ID" />
-
+              
               <div className="col-sm">
                 <span className="titleText">Phone Number</span>
-                <input
+                {/* <input
                   type="tel"
                   id="phone"
                   required="required"
                   className="form-control"
-                />
+                /> */}
+                {phoneInputWithCountryCodes()}
               </div>
             </div>
           </div>
@@ -51,13 +82,29 @@ export default function ReservationForm() {
           {/* Third row */}
           <div className="container">
             <div className="row mt-3 mx-3">
-              <DropDownBar placeholder="Please select a block" title="Block" optionData={testingData} />
-              <DropDownBar placeholder="Please select a time" title="Time Limit for this reservation" optionData={testingData}/>
+              <DropDownBar
+                placeholder="Please select a block"
+                title="Block"
+                optionData={testingData}
+              />
+              <DropDownBar
+                placeholder="Please select a time"
+                title="Time Limit for this reservation"
+                optionData={testingData}
+              />
             </div>
 
             <div className="row mt-3 mx-3">
-              <DropDownBar placeholder="Please select a level" title="Level no." optionData={testingData}/>
-              <DropDownBar placeholder="Please select an unit" title="Unit no." optionData={testingData}/>
+              <DropDownBar
+                placeholder="Please select a level"
+                title="Level no."
+                optionData={testingData}
+              />
+              <DropDownBar
+                placeholder="Please select an unit"
+                title="Unit no."
+                optionData={testingData}
+              />
             </div>
           </div>
 
